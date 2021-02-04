448becde06f16ccc0c24ab473c6caa9d7b71b236
dhe <dhe@costar.com>
2021-02-04 14:25:52 -0800

    test pass local data

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<svelte:options tag="my-app" />
+<script lang="ts">
+	import { onMount } from "svelte";
+	import {} from './Localizer.svelte';
+
+	export let local:string;
+	
+	let name:string;
+	
+	let localComponent;
+
+	onMount(()=>{
+		name = localComponent.getOne('name');
+	});
+</script>
+
+<h1>Hello {name}!</h1>
+
+<localizer-link data={local} bind:this={localComponent}></localizer-link>
---
+++ src/Localizer.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag="localizer-link" />
+<script lang="ts">
+    export let data:string;
+    console.info('local data', data);
+
+    export function getAll(){
+		return 'test'
+	}	
+
+    export function getOne(key:string){
+        var localData = JSON.parse(data);
+		return localData[key];
+	}
+</script>
