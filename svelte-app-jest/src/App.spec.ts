c76c54a8f0a5c424ee789f66efa585f3fdd6fdc3
dhe <keke@dhes-mbp.lan>
2021-02-08 19:43:29 -0800

    typescript support

--- svelte-app-jest/src/App.spec.ts
+++ svelte-app-jest/src/App.spec.ts
@@ -2,7 +2,9 @@ import { render } from "@testing-library/svelte";
 import App from "./App.svelte";
 
 test("should render", () => {
-  const results = render(App, { props: { name: "world" } });
+  const _name: string = 'world';
+
+  const results = render(App, { props: { name: _name } });
 
   expect(() => results.getByText("Hello world!")).not.toThrow();
 });
