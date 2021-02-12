eea4675fbded7140e397418a4c10e3309b89b66d
dhe <keke@dhes-mbp.lan>
2021-02-12 00:26:04 -0800

    test

--- svelte-app-jest/src/App.spec.js
+++ svelte-app-jest/src/App.spec.js
@@ -1,8 +1,8 @@
-import { render, fireEvent } from '@testing-library/svelte';
-import App from './App.svelte'
+import { render } from '@testing-library/svelte';
+import App from './App.svelte';
 
 it(App.name, async () => {
-    const results = render(App, { props: { name: "world" } });
+  const results = render(App, { props: { name: 'world' } });
 
-    expect(() => results.getByText("Hello world!")).not.toThrow();
+  expect(() => results.getByText('Hello world!')).not.toThrow();
 });
