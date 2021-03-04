a84d0853ccd4b906af2326105725dcb281d7b0d7
dhe <keke@dhes-mbp.lan>
2021-03-03 21:54:09 -0800

    add cypress

---
+++ svelte-template-jest-typescript-cypress/src/App.e2e.ts
@@ -0,0 +1,13 @@
+import App from './App.svelte'
+import { mount } from 'cypress-svelte-unit-test'
+it('Test App.svelte ', () => {
+  // mount(App, {
+  //   props: {
+  //     name: 'World',
+  //   },
+  // })
+
+  
+
+  cy.get('my-app').shadow().contains('h1', 'Hello World!');
+})
